591568236106293125e84c986065c34f41e957a4
Zuhaib Siddique <zuhaib.siddique@leapfile.com>
2012-06-06 23:41:20 -0700

    changes

--- start.py
+++ start.py
@@ -37,7 +37,7 @@ def sendToXbmc(listing):
     
     for item in listing:
         listItem = xbmcgui.ListItem(item)
-        xbmcplugin.addDirectoryItem(pluginId,'/Users/zuhaib/Downloads/Doctor.Who.2005.6x13.The.Wedding.Of.River.Song.HDTV.XviD-FoV.avi.strm',listItem)
+        xbmcplugin.addDirectoryItem(pluginId,'/Users/zuhaib/Downloads/Movie Nov 03, 7 22 36 PM.mov.strm',listItem)
         
     xbmcplugin.endOfDirectory(pluginId)
     
