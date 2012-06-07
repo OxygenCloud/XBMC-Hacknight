8372e29c764cadee90d1982926ea48eb853f48ae
Zuhaib Siddique <zuhaib.siddique@leapfile.com>
2012-06-07 01:38:18 -0700

    changes

--- start.py
+++ start.py
@@ -37,7 +37,7 @@ def sendToXbmc(listing):
     
     for item in listing:
         listItem = xbmcgui.ListItem(item)
-        xbmcplugin.addDirectoryItem(pluginId,'/Users/zuhaib/Desktop/Movie Nov 03, 7 22 36 PM.mov.strm',listItem)
+        xbmcplugin.addDirectoryItem(pluginId,'https://i-186eb061.oxygencloud.com/storagegateway/download?c=ed8be4a7-f8a4-4836-9959-1af00e36d983&s=519422ed095f7a6216ea9c2c30fec587c41fe6e4&h=c49004b2a94ce7e76991ee85ed117248d8931f16',listItem)
         
     xbmcplugin.endOfDirectory(pluginId)
     
