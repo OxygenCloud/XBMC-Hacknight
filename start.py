571562e536a22f1e41bb39144971ebca44667c15
Zuhaib Siddique <zuhaib.siddique@leapfile.com>
2012-06-07 17:22:53 -0700

    Adding Java Jar file for API calls to Oxygen and having the python app call it

--- start.py
+++ start.py
@@ -11,18 +11,18 @@ PLUGIN_ID = "plugin.video.oxygencloud"
 pluginId = int(sys.argv[1])
 
 def createListing():
-    cwd = os.getcwd
-    os.system ('%d/java -jar runtime.jar' % cwd)
+    cwd = os.getcwd()
+    os.system ('java -jar %s/runtime.jar' % cwd)
     listing = []
     listing.append('Video')
     return listing
     
 def sendToXbmc(listing):
     global pluginId
-    
+    home = os.getenv('HOME')
     for item in listing:
         listItem = xbmcgui.ListItem(item)
-        xbmcplugin.addDirectoryItem(pluginId,'~/.nancy/',listItem,True)
+        xbmcplugin.addDirectoryItem(pluginId,'%s/.nancy/' % home,listItem,True)
         
     xbmcplugin.endOfDirectory(pluginId)
     
